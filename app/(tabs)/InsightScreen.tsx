d2ea96aaba1ab75166d2805feaf4596bcd8d2698
raskhan <raskanr.j07@gmail.com>
2023-12-30 21:21:43 +0530

    healthy-tips

--- app/(tabs)/InsightScreen.tsx
+++ app/(tabs)/InsightScreen.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import StopWatch from '../Componets/InSightScreenComponents/StopWatch'
+import HealthyTips from '../Componets/InSightScreenComponents/HealthyTips'
 
 const InsightScreen = () => {
   return (
     <View style={[StyleSheet.absoluteFill,{backgroundColor:"#1C1C1E"}]}>
         <View style={{width:"100%",marginTop:10,alignItems:"center"}}>
           <StopWatch />
+        </View>
+        <View style={{width:"100%",marginTop:10,alignItems:"center",flexDirection:"row"}}>
+          <HealthyTips />
         </View>
     </View>
   )
